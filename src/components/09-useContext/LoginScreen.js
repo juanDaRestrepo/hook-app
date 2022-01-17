3d88283d11dd24d02b1f5a985bc17694c18ad99f
juanDaRestrepo <jdrestrepo@unitecnica.net>
2022-01-17 11:59:50 -0500

    Trabajando con useContext

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React,{useContext} from 'react'
+import { UserContext } from './UserContext'
 
 export const LoginScreen = () => {
+    const { setUser} = useContext(UserContext);
+    const us = {
+        id:234,
+        name: 'Juan Daniel'
+    }
     return (
         <div>
             <h1>LoginScreen</h1>
             <hr />
+
+            <button
+                className='btn btn-primary'
+                onClick={ () => setUser(us)}>
+                Login
+            </button>
         </div>
     )
 }
--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { AppRouter } from './AppRouter'
+import { UserContext } from './UserContext'
+
 
 export const MainApp = () => {
+    
+   const [user, setUser] = useState({})
+
     return (
-        <div>
-            <AppRouter />
-        </div>
-    )
+            <UserContext.Provider value={{
+                    user,
+                    setUser,
+                    temporal: 1234
+                }}>
+                <AppRouter />
+            </UserContext.Provider>
+            )
 }
